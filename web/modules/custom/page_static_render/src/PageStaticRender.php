4956c7eae561e08c3af5434ad9df7c73e346671c
kato83 <katomeganeb83@gmail.com>
2023-03-04 19:44:28 +0900

    調整

--- web/modules/custom/page_static_render/src/PageStaticRender.php
+++ web/modules/custom/page_static_render/src/PageStaticRender.php
@@ -3,6 +3,7 @@
 namespace Drupal\page_static_render;
 
 use Drupal\node\Entity\Node;
+use Drupal\path_alias\Entity\PathAlias;
 
 class PageStaticRender
 {
@@ -30,8 +31,44 @@ class PageStaticRender
     // exec("php -f /opt/drupal/scripts/test.php > /dev/null 2>&1 &");
   }
 
+  /**
+   * ファイル削除
+   * @todo PHP 標準のファイル削除処理に置き換える
+   */
   public function deleteByPath(string $path)
   {
     exec("rm -f \"/opt/drupal/html$path/index.html\" > /dev/null 2>&1 &");
+  }
+
+  public function nodeUpsertCheck(Node $node)
+  {
+    $moderation_state = $node->get('moderation_state')->getString();
+    /** @var \Drupal\page_static_render\PageStaticRender $render */
+    $render = \Drupal::service('page_static_render.render');
+    $logger = \Drupal::logger('page_static_render_node_insert');
+
+    if ($moderation_state === 'unpublish') {
+      $logger->info('delete');
+      page_static_render_node_delete($node);
+    } else if ($moderation_state === 'published') {
+      /** @var \Drupal\page_static_render\PageStaticRender $render */
+      $render = \Drupal::service('page_static_render.render');
+      $render->renderNode($node);
+    }
+  }
+
+  /**
+   * パス移動が実施されたかのチェック
+   */
+  public function pathAliasMoveCheck(PathAlias $pathAlias)
+  {
+    $result = \Drupal::entityQuery('path_alias')
+      ->accessCheck(true)
+      ->condition('path', $pathAlias->getPath())
+      ->execute();
+    $path_alias = PathAlias::loadMultiple($result);
+
+    if (!empty($path_alias))
+      $this->deleteByPath(array_values($path_alias)[0]->getAlias());
   }
 }
