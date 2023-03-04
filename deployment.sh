40de16090951c92cab29eba1593d1e39af8d6bb6
kato83 <katomeganeb83@gmail.com>
2023-03-05 03:32:55 +0900

    画像周りの調整

---
+++ deployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PID=$$
+
+echo $PID > /tmp/deployment.pid
+echo "PROCESS ID: $PID"
+/usr/bin/sleep 3
+
+DEBOUNCE=$(cat /tmp/deployment.pid)
+echo "RESULT: $PID, $DEBOUNCE"
+
+if [ $PID = $DEBOUNCE ]; then
+  echo 'S3 UPLOAD'
+  /usr/local/bin/aws s3 sync --profile ${AWS_CLI_PROFILE} /root/deployment/ s3://${AWS_S3_BUCKET}/ --delete
+else
+  echo 'SKIP S3 UPLOAD'
+fi
--- web/modules/custom/page_static_render/src/Commands/Render.php
+++ web/modules/custom/page_static_render/src/Commands/Render.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\page_static_render\Commands;
 
+use Drupal\Component\Utility\Html;
 use Drupal\node\Entity\Node;
 use Drush\Commands\DrushCommands;
+use IvoPetkov\HTML5DOMDocument;
 
 /**
  * A drush command file.
@@ -29,7 +31,34 @@ class Render extends DrushCommands
     \Drupal::logger('info')->info($url);
     if (!file_exists("/opt/drupal/html$url")) mkdir("/opt/drupal/html$url", 0775, true);
     exec("curl -s -H 'Host: www.pu10g.com' \"http://127.0.0.1$url\" -o \"/opt/drupal/html$url/index.html\"");
+    $html = file_get_contents("http://127.0.0.1$url");
+    $images = $this->findImageByHtml($html);
+    foreach ($images as $value) {
+      $this->deployImage($value);
+    }
+
     $this->output()->writeln("OK");
+  }
+
+  private function deployImage(string $url)
+  {
+    if (!file_exists("/opt/drupal/html$url")) mkdir(dirname("/opt/drupal/html$url"), 0775, true);
+    $url = parse_url($url, PHP_URL_PATH);
+    \Drupal::logger('image')->info("curl -s -H 'Host: www.pu10g.com' \"http://127.0.0.1$url\" -o \"/opt/drupal/html$url\"");
+    exec("curl -s -H 'Host: www.pu10g.com' \"http://127.0.0.1$url\" -o \"/opt/drupal/html$url\"");
+  }
+
+  private function findImageByHtml(string $html): array
+  {
+    $document = new HTML5DOMDocument();
+    $document->loadHTML($html);
+    $imgs = $document->querySelectorAll('article img');
+    $results = [];
+    /** @var \IvoPetkov\HTML5DOMElement $img */
+    foreach ($imgs as $img) {
+      $results[] = $img->getAttribute('src');
+    }
+    return array_filter($results, 'strlen');
   }
 
   /**
