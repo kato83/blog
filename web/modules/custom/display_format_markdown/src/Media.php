3613004c9bc0117961e016011e149f814de846cb
kato83 <katomeganeb83@gmail.com>
2023-03-10 00:55:16 +0900

    Media#getId Media#setId 復活

--- web/modules/custom/display_format_markdown/src/Media.php
+++ web/modules/custom/display_format_markdown/src/Media.php
@@ -11,6 +11,17 @@ class Media extends AbstractBlock
   public function __construct(int $id)
   {
     parent::__construct();
+
+    $this->id = $id;
+  }
+
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  public function setId(int $id): void
+  {
     $this->id = $id;
   }
 }
