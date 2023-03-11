503dea22afe837b4c6d76fa891b580c366c1ead2
kato83 <katomeganeb83@gmail.com>
2023-03-11 15:03:16 +0900

    cloudfront キャッシュの実装調整

--- deployment.sh
+++ deployment.sh
@@ -12,6 +12,7 @@ echo "RESULT: $PID, $DEBOUNCE"
 if [ $PID = $DEBOUNCE ]; then
   echo 'S3 UPLOAD'
   /usr/local/bin/aws s3 sync --profile ${AWS_CLI_PROFILE} /root/deployment/ s3://${AWS_S3_BUCKET}/ --delete
+  /usr/local/bin/aws cloudfront create-invalidation --distribution-id ${AWS_CF_ID} --paths "/*"
 else
   echo 'SKIP S3 UPLOAD'
 fi
